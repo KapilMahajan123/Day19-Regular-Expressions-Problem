c40d9c297b6331fc8bcc40c532778cf670384ffd
kapilmahajan123 <montyempire123gmail.com>
2022-12-21 09:44:13 +0530

    Added: To Validation of First Name

---
+++ src/com/Regex/UserRegistration.java
@@ -0,0 +1,27 @@
+package com.Regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * @author Kapil
+ *
+ */
+public class UserRegistration {
+	
+	public boolean userName(String regex , String input) {
+		return Pattern.compile(regex).matcher(input).matches();
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First Name :");
+		String firstName = "[A-Z]{1}[a-z]{2,}";
+		String input = scanner.next(); //Kapil = true  //kapil = false
+		UserRegistration userRegistration = new UserRegistration();
+		System.out.println(userRegistration.userName(firstName, input));
+
+	}
+
+}
