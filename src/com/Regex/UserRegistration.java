5939be63095e2a6bd0a63b9f0ddf4857fadaff26
kapilmahajan123 <montyempire123gmail.com>
2022-12-21 11:49:52 +0530

    Added: At Least One Numeric

--- src/com/Regex/UserRegistration.java
+++ src/com/Regex/UserRegistration.java
@@ -45,11 +45,12 @@ public class UserRegistration {
 		/*
 		 * As a user need to add Password rules
 		 * At a minimum 8 characters
-		 * And maximum any character 
+		 * And maximum any character and should have at least one numeric number
 		 */
 		System.out.println("Enter Password");
 		//String password = "[A-Za-z0-9]{8}$";
-		String password = "((?=.*[A-Z]).{8,})";
+		//String password = "((?=.*[A-Z]).{8,})";
+		String password = "((?=.*[A-Z])(?=.*[0-9]).{8,})";
 		String inputPassword = scanner.next(); 
 		
 		UserRegistration userRegistration = new UserRegistration();
